b41da3d4a5cd0be54ea847d76795014446434aaf
hussam asfour <hussam_asfour@yahoo.com>
2021-04-15 19:38:28 -0400

    initial commit

---
+++ src/main/java/com/hussam/prisonMang/PrisonManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hussam.prisonMang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrisonManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrisonManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hussam/prisonMang/PrisonManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hussam.prisonMang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrisonManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
